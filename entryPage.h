d8585eb1c3408f59a56cf68336fd0e3b982b2ac0
pageap <77745069+pageap@users.noreply.github.com>
2021-04-26 15:05:07 -0500

    Create entryPage.h

---
+++ entryPage.h
@@ -0,0 +1,13 @@
+/*
+Programmer: Adam Page
+Programmer ID
+Class: csci480
+Assignment#: 5
+Date Due: 11/29/2020
+*/
+struct  entryPage
+{
+//create bool statement for valid
+bool valid;
+int physicalMemoryIndex;
+};
